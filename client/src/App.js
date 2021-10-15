5953d92a6cfeae94ee80e4794fa71c04b802c03c
Eric Duwe <ericduwe@gmail.com>
2021-10-14 21:09:28 -0500

    updated StoreProvider function in GlobalState

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider store={store}>
+          <StoreProvider>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
